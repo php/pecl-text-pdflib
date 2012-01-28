1396eecc25d73d410501f4b08d650d04d94f6a3f
Rasmus Lerdorf <rasmus@php.net>
2012-01-28 03:05:18 +0000

    PHP 5.4 support from fedora at famillecollet dot Fixes bug #60397
    git-svn-id: http://svn.php.net/repository/pecl/pdf/trunk@322900 c90b9560-bf6c-de11-be94-00142212c4b1

--- pdf7.c
+++ pdf7.c
@@ -332,10 +332,14 @@ pdflib_object_new_ex(zend_class_entry *class_type, pdflib_object**obj TSRMLS_DC)
 
     ALLOC_HASHTABLE(intern->std.properties);
     zend_hash_init(intern->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
+#if PHP_VERSION_ID < 50399
     zend_hash_copy(intern->std.properties,
             &class_type->default_properties,
             (copy_ctor_func_t) zval_add_ref,
             (void *) &tmp, sizeof(zval *));
+#else
+    object_properties_init( (zend_object*)intern, class_type );
+#endif
 
     retval.handle = zend_objects_store_put(intern,  NULL,
             (zend_objects_free_object_storage_t)pdflib_object_dtor,
